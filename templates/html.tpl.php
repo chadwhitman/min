84dc09db288cfa156bb284fcdde73c66916735d7
Chad Whitman <chad.whitman@icloud.com>
2014-03-20 16:34:36 -0400

    Updating HTML file.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,36 +1,51 @@
 <!DOCTYPE html>
-<!--[if lt IE 7 ]>              <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]>                 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]>                 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> class="no-js ie8"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!-->
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title><?php print $head_title; ?></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		 <!-- Styles  -->
+		 <?php print $styles; ?>
+        <link rel="stylesheet" href="http://localhost:8888/min/sites/all/themes/min/css/normalize.css">
+        <link rel="stylesheet" href="http://localhost:8888/min/sites/all/themes/min/css/all.css">
+        <link rel="stylesheet" href="http://localhost:8888/min/sites/all/themes/min/css/main.css">
 
-<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> class="no-js"> <!--<![endif]-->
+        <!-- Google Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Coda:800' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Crimson+Text' rel='stylesheet' type='text/css'>
 
-<head profile="<?php print $grddl_profile; ?>">
-  <?php print $head; ?>
-  <!-- TODO: add the following meta tags to $head via theme_settings -->
-  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-       Remove this if you use the .htaccess -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <script src="http://localhost:8888/min/sites/all/themes/min/js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+   <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
 
-  <!--  Mobile viewport optimized: j.mp/bplateviewport -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <div id="skip-link"> <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a></div>
 
-  <title><?php print $head_title; ?></title>
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
-  <link href='http://fonts.googleapis.com/css?family=Coda:800' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Crimson+Text' rel='stylesheet' type='text/css'>
+         <?php print $page_top; ?>
+		 <?php print $page; ?>
+		 <?php print $page_bottom; ?>
 
-
-</head>
-<body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-  </div>
-  <?php print $page_top; ?>
-  <?php print $page; ?>
-  <?php print $page_bottom; ?>
-  <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
-</body>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="http://localhost:8888/min/sites/all/themes/min/js/plugins.js"></script>
+        <script src="http://localhost:8888/min/sites/all/themes/min/js/main.js"></script>
+        <?php print $scripts; ?>
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
 </html>
+
