34253566118dd9c69edabb96bf1db0235e5421d4
chadwhitman <chad.whitman@icloud.com>
2014-03-24 00:15:50 -0400

    Updating SASS.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -23,7 +23,7 @@
             </h1>
           <?php endif; ?>
 
-            <h2 id="site-slogan">
+            <div id="menu">
             <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t(''))); ?> </h2>
         </hgroup>
        <?php endif; ?>
