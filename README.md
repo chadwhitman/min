63519c6a1a99a4a98d4117d9873e2f1edc229fe9
Chad Whitman <chad.whitman@icloud.com>
2014-04-10 11:25:39 -0400

    Updating SASS and template.php file.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Min Theme: A Personal Blog theme for Drupal 7
-
+Version: 1.2
+License: GPL => http://www.gnu.org/copyleft/gpl.html
 Description: Min is a minimal, responsive, single-column, content based theme.
+Uses: HTML5 Boilerplate, SASS/SCSS, Normalize.css
 --------------------------------------------
 
 OVERVIEW
--- template.php
+++ template.php
@@ -3,3 +3,14 @@
  * @file
  * Override a theme's functions here
  */
+
+function min_preprocess_html(&$variables) {
+  drupal_add_css(
+  'http://fonts.googleapis.com/css?family=Lato',
+  array('type' => 'external')
+  );
+  drupal_add_css(
+  'http://fonts.googleapis.com/css?family=Crimson+Text',
+  array('type' => 'external')
+  );
+}
